688f9503506d04cc62e9db6a34962bf17ee3f4b9
Ramiro Berbetoros <ramirober@MacBook-Air-de-Ramiro.local>
2025-05-08 10:03:34 -0300

    Fixed prettierd configs

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -7,16 +7,15 @@ return {
 		local null_ls = require("null-ls")
 		null_ls.setup({
 			sources = {
-				-- require("none-ls.diagnostics.eslint_d"),
 				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.formatting.clang_format,
 				null_ls.builtins.formatting.prettierd.with({
 					extra_args = {
 						"--single-quote",
 						"--jsx-single-quote",
-						"--trailing-comma" == "es5",
-						"--arrow-parens" == "avoid",
-						"--tab-width" == 2,
+						"--trailing-comma=es5",
+						"--arrow-parens=avoid",
+						"--tab-width=2",
 					},
 				}),
 			},
