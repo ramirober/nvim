5a5e540031ffe95bf046d1bbb4f7e34c4fcaf052
Ramiro Berbetoros <ramiroberbetoros@gmail.com>
2025-05-13 13:19:24 -0300

    lazygit config update

--- lua/plugins/lazygit.lua
+++ lua/plugins/lazygit.lua
@@ -8,13 +8,17 @@ return {
     "LazyGitFilter",
     "LazyGitFilterCurrentFile",
   },
-  -- optional for floating window border decoration
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-  },
   -- setting the keybinding for LazyGit with 'keys' is recommended in
   -- order to load the plugin when the command is run for the first time
   keys = {
     { "<leader>1", "<cmd>LazyGit<cr>", desc = "LazyGit" },
   },
+  -- optional for floating window border decoration
+  dependencies = {
+    "nvim-telescope/telescope.nvim",
+    "nvim-lua/plenary.nvim",
+  },
+  config = function()
+    require("telescope").load_extension("lazygit")
+  end,
 }
