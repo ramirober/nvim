b58467baf8d5098ce3c801655cf6723970bd46e2
ramirober <ramiroberbetoros@icloud.com>
2025-06-05 15:57:23 -0300

    Updates

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -4,6 +4,7 @@ return {
     require("lualine").setup({
       options = {
         theme = "catppuccin",
+        -- theme = "auto",
         -- component_separators = { left = "", right = "" },
         -- section_separators = { left = "", right = "" },
         component_separators = { left = "|", right = "|" },
