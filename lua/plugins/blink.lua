b05655b6ef50e0a9ef141773f4b661f36d7a5d72
ramirober <ramiroberbetoros@icloud.com>
2026-04-14 09:20:36 -0300

    Warning disable for blink.lua

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -13,6 +13,7 @@ return {
 	-- build = 'nix run .#build-plugin',
 
 	---@module 'blink.cmp'
+	---@diagnostic disable-next-line: undefined-doc-name
 	---@type blink.cmp.Config
 	opts = {
 		-- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
